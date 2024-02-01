4bf6f246f94049d1331024fcdd434c4dda705490
Divam484 <pateldivam2212@gmail.com>
2024-02-01 18:02:02 +0530

    initial commit

--- controller/school.js
+++ controller/school.js
@@ -1,6 +1,6 @@
 const db = require("../model/index");
 // const jwt = require('jsonwebtoken');
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
 
